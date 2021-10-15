f6301d8724da131a6f0436275daedeafa7003a0b
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-15 16:59:44 +0700

    fix(parsedConfig): yml file wont parsed

--- src/utils/parsedConfig.ts
+++ src/utils/parsedConfig.ts
@@ -1,5 +1,6 @@
 import yaml from 'js-yaml';
 import { join } from 'path';
 import { configType } from '../types';
-const importYaml = yaml.load(join(__dirname, '..', 'config.yml')) as configType;
+import { readFileSync } from 'fs';
+const importYaml = yaml.load(readFileSync(join(__dirname, '..', 'config.yml'), "utf8")) as configType;
 export const config = importYaml;
