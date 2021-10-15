0d41107460fd329ea626f6dafc175d907209b6de
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-15 17:25:20 +0700

    feat(clientReady): add botOwner logging & add client activity

--- src/listeners/clientReady.ts
+++ src/listeners/clientReady.ts
@@ -14,9 +14,30 @@ export class clientListener extends Listener {
         if (developerId?.owner instanceof Team) {
             for (const [ownerId] of developerId?.owner.members) {
                 if (!config.botOwners.includes(ownerId)) config.botOwners.push(ownerId);
+                try {
+                    const fetchedUser = await this.container.client.users.fetch(ownerId)
+                    this.container.client.logger.info(`${green("[Client]")} ${magentaBright(`registered ${fetchedUser.username} as bot owner`)}`);
+                } catch(e) {
+                    if (!config.botOwners.includes(ownerId)) config.botOwners.push(ownerId);
+                    this.container.client.logger.info(`${green("[Client]")} ${magentaBright(`registered ${ownerId} as bot owner, but failed to fetch`)}`);
+                    continue;
+                }
                 continue;
             }
-        } else if (!config.botOwners.includes(developerId?.owner?.id!)) { config.botOwners.push(developerId?.owner?.id!); }
+        } else if (!config.botOwners.includes(developerId?.owner?.id!)) { 
+            try {
+                const fetchedUser = await this.container.client.users.fetch(developerId?.owner?.id!)
+                this.container.client.logger.info(`${green("[Client]")} ${magentaBright(`registered ${fetchedUser.username} as bot owner`)}`);
+                if (!config.botOwners.includes(developerId?.owner?.id!)) config.botOwners.push(developerId?.owner?.id!); 
+            } catch(e) {
+                if (!config.botOwners.includes(developerId?.owner?.id!)) config.botOwners.push(developerId?.owner?.id!);
+                    this.container.client.logger.info(`${green("[Client]")} ${magentaBright(`registered ${developerId?.owner?.id!} as bot owner, but failed to fetch`)}`);
+            }
+        }
+        this.container.client.user?.setActivity({
+            name: (this.container.client.application?.commands.cache.size ? "+play" : "/play") + ` | shard ${this.container.client.shard?.ids[0] ?? 1}/${this.container.client.shard?.ids.length ?? 1}`,
+            type: this.container.client.application?.commands.cache.size ? "WATCHING" : "LISTENING"
+        })
         this.container.client.logger.info(`${green("[Client]")} ${magentaBright(`${this.container.client.user!.username} ready with ${this.container.client.guilds.cache.size} guilds`)}`);
     }
 }
