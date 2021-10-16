dc24f340e94cb5cbf4d51acd60de052178e4917e
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-16 10:45:44 +0700

    chore(*): update config file

--- src/utils/parsedConfig.ts
+++ src/utils/parsedConfig.ts
@@ -2,5 +2,6 @@ import yaml from 'js-yaml';
 import { join } from 'path';
 import { configType } from '../types';
 import { readFileSync } from 'fs';
-const importYaml = yaml.load(readFileSync(join(__dirname, '..', 'config.yml'), "utf8")) as configType;
+const importYaml = yaml.load(readFileSync(join(__dirname, '..', '..', 'config.yml'), "utf8")) as configType;
 export const config = importYaml;
+ 
