23c58893ff7c34a8bd7b26ff1bc9374ac7e32c1a
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-17 09:57:02 +0700

    chore(index): configure sapphire logger

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import { Shard, ShardingManager } from "discord.js";
 import { join } from "path";
 import "@sapphire/plugin-logger/register";
 import * as colorette from "colorette";
+import { Logger } from "@sapphire/plugin-logger";
 colorette.createColors({ useColor: true });
-
+const logger = new Logger();
 const shardClient = new ShardingManager(join(__dirname, "structures", "NezuClient.js"), {
     respawn: true
 });
 
 shardClient.on("shardCreate", (shard: Shard) => {
-    console.info(`${colorette.green("[Shard]")} ${colorette.magentaBright(`Launched shard with Id: ${shard.id}`)}`);
+    logger.info(`${colorette.green("[Shard]")} ${colorette.magentaBright(`Launched shard with Id: ${shard.id}`)}`);
 });
 
 shardClient.spawn().catch((error: Error) => console.info("Failed to spawn shard: %d", error.message));
