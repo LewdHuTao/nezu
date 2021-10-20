947bd4b6769a6efb6b49de67c268ce5499cd231a
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-20 14:03:38 +0700

    chore(JoinCommand): add mising required permissions

--- src/commands/Music/JoinCommand.ts
+++ src/commands/Music/JoinCommand.ts
@@ -2,18 +2,20 @@ import { CommandOptions, Command } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
 import { Message, MessageEmbed } from "discord.js";
 import { GuildTextBasedChannelTypes } from "@sapphire/discord.js-utilities";
+import { isEligbleReply } from "../../utils/isEligbleReply";
 
 @ApplyOptions<CommandOptions>({
     name: "join",
     description: "let bot join current user voice channel",
     preconditions: ["threadCondition", "onVoiceCondition", "alreadyConnect"],
-    requiredClientPermissions: ["SEND_MESSAGES"]
+    requiredClientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"]
 })
 
 export class clientCommand extends Command {
     async messageRun(message: Message) {
         await this.container.client.audioManager.handleJoin(message.member?.voice.channel!, message.channel as GuildTextBasedChannelTypes);
         await message.channel.send({
+            reply: isEligbleReply(message),
             embeds: [
                 new MessageEmbed()
                     .setDescription(`✅ | Joined ${message.member?.voice.channel!.toString()} voice channel`)
