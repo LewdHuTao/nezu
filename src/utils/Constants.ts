589c8d8fd2a26726057b61c002ebbc75d3e58dc7
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-21 11:00:12 +0700

    chore(Constants): add new emoji

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,5 +1,6 @@
 export const emoji = {
     Music: "<:nezuko_woah:808650707737509918>",
     Utility: "<:nezuko_pat:808651540420100109>",
-    Playlist: "<:nezuko_peek:808651352997756938>"
+    Playlist: "<:nezuko_peek:808651352997756938>",
+    Filters: "<:zenitsu_love:808655844451156008>"
 };
