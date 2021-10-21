75df2f7aa9b45923acfa370d22507b2553be599d
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-21 21:45:35 +0700

    fix(DisconnectCommand): queue not deleted if disconnected

--- src/commands/Music/DisconnectCommand.ts
+++ src/commands/Music/DisconnectCommand.ts
@@ -14,6 +14,7 @@ import { isEligbleReply } from "../../utils/isEligbleReply";
 export class clientCommand extends Command {
     async messageRun(message: Message) {
         this.container.client.audioManager.queue.get(message.guildId!)?.shoukakuPlayer.connection.disconnect();
+        this.container.client.audioManager.queue.delete(message.guildId!)
         await message.channel.send({
             reply: isEligbleReply(message),
             embeds: [
