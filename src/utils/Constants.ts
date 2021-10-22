44c61488339ef9b5b52fe5ac419d23552b6bad74
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-22 09:32:30 +0700

    chore(Constants): add missing emoji

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -2,5 +2,6 @@ export const emoji = {
     Music: "<:nezuko_woah:808650707737509918>",
     Utility: "<:nezuko_pat:808651540420100109>",
     Playlist: "<:nezuko_peek:808651352997756938>",
-    Filters: "<:zenitsu_love:808655844451156008>"
+    Filters: "<:zenitsu_love:808655844451156008>",
+    Reactions: "<:nezuko_hype:808653993256681503>"
 };
