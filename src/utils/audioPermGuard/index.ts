1d9bdc5c618daf6265edb1d0634b8759b7db93c4
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-26 20:07:15 +0700

    fix(audioPermGuard): change checking logic

--- src/utils/audioPermGuard/index.ts
+++ src/utils/audioPermGuard/index.ts
@@ -1,6 +1,7 @@
-import { isGuildBasedChannel, isThreadChannel } from '@sapphire/discord.js-utilities';
-import { Nullish } from '@sapphire/utilities';
-import { TextBasedChannels } from 'discord.js';
+import { isGuildBasedChannel, isThreadChannel } from "@sapphire/discord.js-utilities";
+import { Nullish } from "@sapphire/utilities";
+import { TextBasedChannels } from "discord.js";
 export function isHasSendPerm(channel: TextBasedChannels | Nullish) {
-    return isThreadChannel(channel) && channel.permissionsFor(channel.guild.me!).has("SEND_MESSAGES_IN_THREADS") || isGuildBasedChannel(channel) && channel.permissionsFor(channel.guild.me!).has("SEND_MESSAGES") 
-} 
+    if (isThreadChannel(channel)) return channel.permissionsFor(channel.guild.me!).has("SEND_MESSAGES_IN_THREADS");
+    else if (isGuildBasedChannel(channel)) return channel.permissionsFor(channel.guild.me!).has("SEND_MESSAGES");
+}
