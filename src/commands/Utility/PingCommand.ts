440f67223c7a910be30ee17c2bf23c008da4531a
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-26 20:09:50 +0700

    chore(PingCommand): display client uptime

--- src/commands/Utility/PingCommand.ts
+++ src/commands/Utility/PingCommand.ts
@@ -1,6 +1,7 @@
 import { CommandOptions, Command } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
 import { Message } from "discord.js";
+import ms from "ms";
 
 @ApplyOptions<CommandOptions>({
     name: "ping",
@@ -12,6 +13,6 @@ import { Message } from "discord.js";
 export class clientCommand extends Command {
     async messageRun(message: Message) {
         const msg = await message.channel.send("Pong");
-        await msg.edit(`Pong :ping_pong: | ${msg.createdTimestamp - message.createdTimestamp} ms, ws ${this.container.client.ws.ping} ms`);
+        await msg.edit(`Pong :ping_pong: | ${msg.createdTimestamp - message.createdTimestamp} ms, ws ${this.container.client.ws.ping} ms, online for ${ms(this.container.client.uptime!, { long: true })}`);
     }
 }
