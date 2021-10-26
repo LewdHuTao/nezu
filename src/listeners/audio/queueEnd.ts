1b06a2cb2861b8b196ab3926460311abd3c66a89
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-26 20:09:07 +0700

    fix(queueEnd): eslint check error

--- src/listeners/audio/queueEnd.ts
+++ src/listeners/audio/queueEnd.ts
@@ -17,7 +17,7 @@ export class clientListener extends Listener {
         player.queueTrack.current = null;
         player.queueTrack.previous = track ?? null;
         if (player.lastMessage) player.lastMessage.delete().catch(() => undefined);
-        if(!isHasSendPerm(player.textChannel)) return;
+        if (!isHasSendPerm(player.textChannel)) return;
         const msg = await player.textChannel.send({
             embeds: [
                 new MessageEmbed()
