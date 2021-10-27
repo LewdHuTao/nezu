0e91a88868b28a5000bd27164de1356354dca267
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-27 15:13:36 +0700

    fix(*): @sapphire/discord.js-utilities breaking change

--- src/commands/Music/Playlist/ListCommand.ts
+++ src/commands/Music/Playlist/ListCommand.ts
@@ -1,8 +1,8 @@
 import { CommandOptions, Command } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
-import { Message, MessageEmbed } from "discord.js";
+import { Constants, Message, MessageEmbed } from "discord.js";
 import { isEligbleReply } from "../../../utils/isEligbleReply";
-import { isGuildBasedChannel, PaginatedMessage } from "@sapphire/discord.js-utilities";
+import { PaginatedMessage } from "@sapphire/discord.js-utilities";
 import { Chunk } from "../../../utils/Chunk";
 import { joinArray } from "../../../utils/joinArray";
 import { inlineCode } from "@discordjs/builders";
@@ -30,52 +30,61 @@ export class clientCommand extends Command {
         }
         const pages = Chunk(getPlaylist.map((x, i) => `\`${i + 1})\` ${x.playlistName} [${inlineCode(x.playlistId)}]`), 7);
         const PageMessage = new PaginatedMessage({
-            template: new MessageEmbed().setThumbnail(message.guild?.iconURL()!).setAuthor(message.guild?.name!, undefined, "https://nezukochan.tech")
-                .setColor("LUMINOUS_VIVID_PINK"),
             actions: [
                 {
-                    id: "⏪",
+                    customId: '@sapphire/paginated-messages.firstPage',
+                    style: 'PRIMARY',
+                    emoji: '⏪',
+                    type: Constants.MessageComponentTypes.BUTTON,
                     run: ({ handler }) => (handler.index = 0)
                 },
                 {
-                    id: "◀️",
+                    customId: '@sapphire/paginated-messages.previousPage',
+                    style: 'PRIMARY',
+                    emoji: '◀️',
+                    type: Constants.MessageComponentTypes.BUTTON,
                     run: ({ handler }) => {
                         if (handler.index === 0) handler.index = handler.pages.length - 1;
                         else --handler.index;
                     }
                 },
                 {
-                    id: "▶️",
+                    customId: '@sapphire/paginated-messages.nextPage',
+                    style: 'PRIMARY',
+                    emoji: '▶️',
+                    type: Constants.MessageComponentTypes.BUTTON,
                     run: ({ handler }) => {
                         if (handler.index === handler.pages.length - 1) handler.index = 0;
                         else ++handler.index;
                     }
                 },
                 {
-                    id: "⏩",
+                    customId: '@sapphire/paginated-messages.goToLastPage',
+                    style: 'PRIMARY',
+                    emoji: '⏩',
+                    type: Constants.MessageComponentTypes.BUTTON,
                     run: ({ handler }) => (handler.index = handler.pages.length - 1)
                 },
                 {
-                    id: "⏹️",
-                    run: async ({ response, collector }) => {
-                        if (
-                            isGuildBasedChannel(response.channel) &&
-                            response.client.user &&
-                            response.channel.permissionsFor(response.client.user.id)?.has("MANAGE_MESSAGES")
-                        ) {
-                            await response.reactions.removeAll();
-                        } else if (isGuildBasedChannel(response.channel) && response.client.user && !response.channel.permissionsFor(response.client.user.id)?.has("MANAGE_MESSAGES")) { return response.delete(); }
+                    customId: '@sapphire/paginated-messages.stop',
+                    style: 'DANGER',
+                    emoji: '⏹️',
+                    type: Constants.MessageComponentTypes.BUTTON,
+                    run: async ({ collector, response }) => {
                         collector.stop();
+                        await response.edit({ components: [] });
                     }
                 }
             ]
         });
         for (const page of pages) {
             PageMessage.addPageEmbed(embed => {
-                embed.setDescription(joinArray(page));
+                embed.setDescription(joinArray(page))
+                embed.setThumbnail(message.guild?.iconURL()!).setAuthor(message.guild?.name!, undefined, "https://nezukochan.tech")
+                embed.setColor("LUMINOUS_VIVID_PINK");
                 return embed;
             });
         }
-        PageMessage.run(message).catch(() => null);
+        await PageMessage.run(message)
     }
 }
